548f5fd53a93a4ff9e0eb37aab614f4f480a5666
shrutijain2296 <128174123+shrutijain2296@users.noreply.github.com>
2023-06-15 09:19:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function Person(name, age) {
 Person.prototype.greet = function(){
 	console.log("Hello, my name is " + this.name + " and I am " + this.age + " years old.");
 }
-console.log(Person)
+
 
 function Employee(name, age, jobTitle) {
 	Person.call(this, name, age);
